1a15c7d2aac7fa0e2abc5050ec2d9cd9c9105260
akshit-rawat-coder <akshitrawat1802@gmail.com>
2026-08-12 21:19:24 +0530

    keep going

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-// import { ErrorWrapper } from "./error-wrapper";
+import { ErrorWrapper } from "./error-wrapper";
 
 export const metadata: Metadata = {
   title: {
@@ -25,9 +25,9 @@ export default function RootLayout({
         >
           <p>Header</p>
         </header>
-        {/* <ErrorWrapper> */}
+        <ErrorWrapper>
         {children}
-        {/* </ErrorWrapper> */}
+        </ErrorWrapper>
         <footer
           style={{
             backgroundColor: "ghostwhite",
